19834211f5dc4ca2589629510f2959d69bcf0753
Sourabh Ahuja <souahuja@paypal.com>
2026-02-11 20:40:14 -0800

    .

--- Learning/src/com/learning/ds/patterns/slidingwindow/verma/P5_CountOccurrencesOfAnagrams.java
+++ Learning/src/com/learning/ds/patterns/slidingwindow/verma/P5_CountOccurrencesOfAnagrams.java
@@ -13,40 +13,6 @@ public class P5_CountOccurrencesOfAnagrams {
     }
 
     private static void countAnagrams(String text, String pattern) {
-        int start = 0;
-        int end = 0;
-        int anagramCounter = 0;
-        int counter = pattern.length();
-        HashMap<Character, Integer> charCountMap = new HashMap<>();
 
-        for(Character c : pattern.toCharArray()) {
-            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
-        }
-        counter = charCountMap.keySet().size();
-
-        while (end < text.length()) {
-            if(charCountMap.get(text.charAt(end)) != null) {
-                charCountMap.put(text.charAt(end), charCountMap.get(text.charAt(end)) - 1);
-                if(charCountMap.get(text.charAt(end)) == 0)
-                    counter--;
-            }
-            if(end - start + 1 < pattern.length())
-                end++;
-            else {
-                if(counter == 0) {
-                    anagramCounter++;
-                }
-
-                //This check is to determine only chars in pattern are considered.
-                if(charCountMap.get(text.charAt(start)) != null) {
-                    if (charCountMap.get(text.charAt(start)) == 0)
-                        counter++;
-                    charCountMap.put(text.charAt(start), charCountMap.get(text.charAt(start)) + 1);
-                }
-                start++;
-                end++;
-            }
-        }
-        System.out.println("Total number of Anagrams: " + anagramCounter);
     }
 }
